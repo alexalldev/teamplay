dc8f5466025daf88524ad63698a9095e4d034900
ilubb <ilubvys@gmail.com>
2019-09-22 00:14:55 +0300

    notifications: invite to team, join to team

--- modules/teamplay-norifications.js
+++ modules/teamplay-norifications.js
@@ -1,54 +1,59 @@
-const notifcationModel = require('./../models/Notification')
-const nodeMailer = require('nodemailer')
+const notifcationModel = require('./../models/Notification');
+const nodeMailer = require('nodemailer');
 
 const User = require('../models/User');
 const fs = require('fs');
 const io = require('../config/sockets/index');
 
 let transporter = nodeMailer.createTransport({
-    host: 'mail.alexall.dev',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'info@teamplay.space',
-        pass: 'teamplayspace'
-    }
-})
+	host: 'mail.alexall.dev',
+	port: 465,
+	secure: true,
+	auth: {
+		user: 'info@teamplay.space',
+		pass: 'teamplayspace'
+	}
+});
 
-module.exports = function(receiverId, header, mainText, isInfoNotification, callback) {
-    User.findOne({where: {UserId: receiverId}, raw: true})
-    .then(user => {
-        if (user)
-        {
-            notifcationModel.create({
-                senderId: 0,
-                receiverId: receiverId,
-                header: header,
-                mainText: mainText,
-                isInfoNotification: isInfoNotification,
-                isRead: false
-            })
-            .then(result => {
-                io.emitUser(receiverId, 'recieveNotification', result.get())
-                callback('true');
-            })
-            .catch(err => {
-                console.log(err)
-            })
-            //Если это заявка, то отправляем на почту
-            if (!isInfoNotification)
-                fs.readFile(__dirname + '/../html_mail/TeamPlayNotificationEmail.html', 'utf-8', function (err, data) {
-                    if (err) callback(err)
-                    var html_mail_array = data.split('NOTIFICATION_TEXT');
-                    var html_mail = html_mail_array[0] + mainText + html_mail_array[1];
-                    transporter.sendMail({
-                        from: 'info@teamplay.space', // sender address
-                        to: user.UserEmail, // list of receivers
-                        subject: header, // Subject line
-                        html: html_mail
-                    })
-                });
-        }
-    })
-    .catch(err => console.log(err))
-}
+//для теста отправляется senderId
+module.exports = function(senderId, receiverId, header, mainText, isInfoNotification, InvitationType, callback, req) {
+	User.findOne({ where: { UserId: receiverId }, raw: true })
+		.then((user) => {
+			if (user) {
+				notifcationModel
+					.create({
+						senderId: senderId,
+						receiverId: receiverId,
+						header: header,
+						mainText: mainText,
+						isInfoNotification: isInfoNotification,
+						isRead: false,
+						InvitationHash: '',
+						InvitationType: InvitationType
+					})
+					.then((result) => {
+						result.dataValues.actionUrl =
+							req.protocol + '://' + req.hostname + '/notification/notificationAction';
+						io.emitUser(receiverId, 'receiveNotification', result.dataValues);
+						callback('true');
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+				//Если это заявка, то отправляем на почту
+				// if (!isInfoNotification)
+				//     fs.readFile(__dirname + '/../html_mail/TeamPlayNotificationEmail.html', 'utf-8', function (err, data) {
+				//         if (err) callback(err)
+				//         var html_mail_array = data.split('NOTIFICATION_TEXT');
+				//         var html_mail = html_mail_array[0] + mainText + html_mail_array[1];
+				//         transporter.sendMail({
+				//             from: 'info@teamplay.space', // sender address
+				//             to: user.UserEmail, // list of receivers
+				//             subject: header, // Subject line
+				//             html: html_mail
+				//         })
+				//     });
+			}
+		})
+		.catch((err) => console.log(err));
+};
--- routes/notifications.js
+++ routes/notifications.js
@@ -1,21 +1,49 @@
-const {
-    router,
-    urlencodedParser
-} = require('../config/routers-config')
+const { router, urlencodedParser } = require('../config/routers-config');
 
 const notification = require('../modules/teamplay-norifications');
+const io = require('../config/sockets/index');
+const notificationModel = require('../models/Notification');
+const userModel = require('../models/User');
 
-router.get('/notification', function (req, res) {
-    res.render('notifications')
-})
+router.get('/notification', function(req, res) {
+	res.render('notifications');
+});
 
-router.post('/sendNotification', urlencodedParser, async function (req, res) {
-    notification(req.body.receiverId, req.body.header, req.body.mainText, true, function(err) {
-        if (err)
-            res.end(JSON.stringify(err));
-        else
-            res.end('true');
-    });
-})
+router.post('/sendNotification', urlencodedParser, function(req, res) {
+	notification(
+		req.body.senderId,
+		req.body.receiverId,
+		req.body.header,
+		req.body.mainText,
+		false,
+		req.body.InvitationType,
+		function(err) {
+			if (err) res.end(JSON.stringify(err));
+			else res.end('true');
+		},
+		req
+	);
+});
 
-module.exports = router
+router.post('/notificationAction', urlencodedParser, function(req, res) {
+	console.log('req.body.InvitationType');
+	console.log(req.body.InvitationType);
+	userModel
+		.findOne({ where: { userId: req.body.senderId }, raw: true })
+		.then((userSender) => {
+			userModel.findOne({ where: { userId: req.body.receiverId }, raw: true }).then((userReceiver) => {
+				io.emitUser(req.body.senderId, 'sendAnswer', {
+					senderFullName: [ userSender.UserName, userSender.UserFamily, userSender.UserLastName ],
+					receiverFullName: [ userReceiver.UserName, userReceiver.UserFamily, userReceiver.UserLastName ],
+					answer: req.body.answer,
+					InvitationType: req.body.InvitationType
+				});
+			});
+		})
+		.then(res.end('true'))
+		.catch((err) => {
+			res.send(err);
+		});
+});
+
+module.exports = router;
