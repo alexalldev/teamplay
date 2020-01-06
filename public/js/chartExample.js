08248839271a78aee017e69ed11935cc416b3338
Ilya Vysotsky <47083790+ilubb@users.noreply.github.com>
2020-01-06 12:44:05 +0300

    data from server is displaying in chart

--- public/js/chartExample.js
+++ public/js/chartExample.js
@@ -1,51 +1,81 @@
-var Color1 = Color2 = Color3 = '#397798';
-var ctx = document.getElementsByClassName('myChart')[0].getContext('2d');
-var chart = new Chart(ctx, {
+$(document).ready(function() {
+  const ptrn = /user\/\d$/i;
+  userUrl = ptrn.exec(window.location.href)[0];
+  socket.emit("GetStatData", userUrl[userUrl.length - 1]);
+});
+socket.on("SendStatData", datesGamesTeamResults => {
+  const Color1 = (Color2 = Color3 = "#397798");
+  const ctx = document.getElementsByClassName("myChart")[0].getContext("2d");
+  const chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'bar',
-
+    type: "bar",
     // The data for our dataset
     data: {
-        labels: ["01—10.09 2019", "11—20.09 2019", "21—30.09 2019", "01—10.10 2019", "11—20.10 2019", "21—31.10 2019"],
-        datasets: [{
-            label: 'количество игр за 10 дней',
-            data: [0, 8, 12, 15, 11, 18],
-            backgroundColor: Color1,
-            borderColor: Color1,
-            borderWidth: 2
-        }]
-    },
-    options:
-    {
-        legend:
+      labels: datesGamesTeamResults.map(
+        dateGameTeamResult =>
+          `${dateGameTeamResult.date.month}, ${dateGameTeamResult.date.year}`
+      ),
+      datasets: [
         {
-            display: true,
-            labels:
-            {
-                fontColor: 'black'
-            }
+          label: "Количество игр в месяц",
+          data: datesGamesTeamResults.map(
+            dateGameTeamResult => dateGameTeamResult.number
+          ),
+          backgroundColor: Color1,
+          borderColor: Color1,
+          borderWidth: 2
         }
+      ]
+    },
+    options: {
+      legend: {
+        display: true,
+        labels: {
+          fontColor: "black"
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+              stepSize: 1.0,
+              stacked: true
+            }
+          }
+        ]
+      }
     }
+  });
+  function RandomColor(rmax, gmax, bmax, opacity) {
+    return `rgb(${Math.round(Math.random() * rmax + 1)}, ${Math.round(
+      Math.random() * gmax + 1
+    )}, ${Math.round(Math.random() * bmax + 1)}, ${opacity})`;
+  }
 
+  function ChangeColorProportionally(RgbColor, Percents) {
+    const colors = String(String(RgbColor.split("(")[1]).split(")")).split(",");
+
+    const opacity = colors[3];
+
+    const R =
+      Math.round((colors[0] * Percents) / 100) >= 0 &&
+      Math.round((colors[0] * Percents) / 100) <= 255
+        ? Math.round((colors[0] * Percents) / 100)
+        : colors[0];
+
+    const G =
+      Math.round((colors[1] * Percents) / 100) >= 0 &&
+      Math.round((colors[1] * Percents) / 100) <= 255
+        ? Math.round((colors[1] * Percents) / 100)
+        : colors[1];
+
+    const B =
+      Math.round((colors[2] * Percents) / 100) >= 0 &&
+      Math.round((colors[2] * Percents) / 100) <= 255
+        ? Math.round((colors[2] * Percents) / 100)
+        : colors[2];
+
+    return `rgb(${R}, ${G}, ${B}, ${opacity})`;
+  }
 });
-
-function RandomColor(rmax, gmax, bmax, opacity) {
-    return 'rgb(' + Math.round(Math.random() * rmax + 1) + ', ' + Math.round(Math.random() * gmax + 1) + ', ' + Math.round(Math.random() * bmax + 1) + ', ' + opacity + ')';
-}
-
-function ChangeColorProportionally(RgbColor, Percents) {
-    var colors = String(String(RgbColor.split('(')[1]).split(')')).split(',');
-
-    var opacity = colors[3];
-
-    var R = Math.round((colors[0] * Percents) / 100) >= 0 && Math.round((colors[0] * Percents) / 100) <= 255
-        ? Math.round((colors[0] * Percents) / 100) : colors[0];
-
-    var G = Math.round((colors[1] * Percents) / 100) >= 0 && Math.round((colors[1] * Percents) / 100) <= 255
-        ? Math.round((colors[1] * Percents) / 100) : colors[1];
-
-    var B = Math.round((colors[2] * Percents) / 100) >= 0 && Math.round((colors[2] * Percents) / 100) <= 255
-        ? Math.round((colors[2] * Percents) / 100) : colors[2];
-
-    return 'rgb(' + R + ', ' + G + ', ' + B + ', ' + opacity + ')';
-}
