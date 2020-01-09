4f30bd1c757228ae8c78c8ffbf3897be55c8052c
Ilya Vysotsky <47083790+ilubb@users.noreply.github.com>
2020-01-09 10:40:45 +0300

    message 'Нет игр' if player wasn't played any game yet

--- public/js/userGameStat.js
+++ public/js/userGameStat.js
@@ -7,65 +7,69 @@ $(".btnUserResults").click(() => {
 });
 
 socket.on("SendUserResults", gameResults => {
-  $.each(gameResults, (i, vi) => {
-    $("#GameResults").append(`
-    <div class="row alert alert-info" data-toggle="collapse"
-      data-target="#GameCollapse-${i}" style="cursor: pointer">
-        <div class="col-md-12">
-          <span>${vi.GameName} ${vi.Timestamp}</span>
-        </div>
-    </div>`);
-    $(`[data-target="#GameCollapse-${i}"]`).after(`
-    <div class="row collapse" id="GameCollapse-${i}">
-      <div class="col-md-12">
-        <table class="table table-hover userResultsTable-${i}">
-            <thead>
-                <th>Текст вопроса</th>
-                <th>Верно/Неверно</th>
-            </thead>
-        </table>
-      </div>
-    </div>
-    `);
-
-    $.each(vi.questions, (j, vj) => {
-      $(`.userResultsTable-${i}`).append(`                
-      <tr>
-      <th>
-          <div class="row" data-toggle="collapse" data-target="#QuestionCollapse-${i}-${j}" style="cursor: pointer">
-              <div class="col-md-12">
-                  <span>${vj.questionText}</span>
-                  <div class="row collapse" id="QuestionCollapse-${i}-${j}">
-                      <div class="col-md-12">
-                          <table class="table table-hover">
-                              <thead class="userQuestionsTable-${i}-${j}">
-                                  <th>Текст ответа</th>
-                                  <th>Верно/Неверно</th>
-                              </thead>
-                          </table>
-                      </div>
-                  </div>
-              </div>
+  if (gameResults.length == 0) {
+    $("#GameResults").append("Нет игр");
+  } else {
+    $.each(gameResults, (i, vi) => {
+      $("#GameResults").append(`
+      <div class="row alert alert-info" data-toggle="collapse"
+        data-target="#GameCollapse-${i}" style="cursor: pointer">
+          <div class="col-md-12">
+            <span>${vi.GameName} ${vi.Timestamp}</span>
           </div>
-      </th>
-      <th>
-        <span>${vj.isAnsweredCorrectly ? "✓" : "☓"}</span>
-      </th>
-  </tr>
-`);
-
-      $.each(vj.answers, (k, vk) => {
-        $(`.userQuestionsTable-${i}-${j}`).after(`
-          <tr>
-            <th>
-              ${vk.answerText}
-            </th>
-            <th>
-             ${vk.isCorrect ? "✓" : "☓"}
-            </th>
-          </tr>
+      </div>`);
+      $(`[data-target="#GameCollapse-${i}"]`).after(`
+      <div class="row collapse" id="GameCollapse-${i}">
+        <div class="col-md-12">
+          <table class="table table-hover userResultsTable-${i}">
+              <thead>
+                  <th>Текст вопроса</th>
+                  <th>Верно/Неверно</th>
+              </thead>
+          </table>
+        </div>
+      </div>
       `);
+
+      $.each(vi.questions, (j, vj) => {
+        $(`.userResultsTable-${i}`).append(`
+        <tr>
+        <th>
+            <div class="row" data-toggle="collapse" data-target="#QuestionCollapse-${i}-${j}" style="cursor: pointer">
+                <div class="col-md-12">
+                    <span>${vj.questionText}</span>
+                    <div class="row collapse" id="QuestionCollapse-${i}-${j}">
+                        <div class="col-md-12">
+                            <table class="table table-hover">
+                                <thead class="userQuestionsTable-${i}-${j}">
+                                    <th>Текст ответа</th>
+                                    <th>Верно/Неверно</th>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </th>
+        <th>
+          <span>${vj.isAnsweredCorrectly ? "✓" : "☓"}</span>
+        </th>
+    </tr>
+  `);
+
+        $.each(vj.answers, (k, vk) => {
+          $(`.userQuestionsTable-${i}-${j}`).after(`
+            <tr>
+              <th>
+                ${vk.answerText}
+              </th>
+              <th>
+               ${vk.isCorrect ? "✓" : "☓"}
+              </th>
+            </tr>
+        `);
+        });
       });
     });
-  });
+  }
 });
